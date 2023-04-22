97296bb3e9867caaf2ebc4e349bf11bcb895e562
Raydan Bala <raydanb@gmail.com>
2023-04-22 12:29:28 -0700

    functional list, need to break up into different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false}
+    ]})
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo;
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return <>
+
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item:</label>
+        <input 
+        value={newItem}
+        onChange={e => {setNewItem(e.target.value)}}
+        type="text"
+        id="item"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input 
+                type="checkbox" 
+                checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.value)}/>
+              {todo.title}
+            </label>
+            <button 
+              onClick={() => deleteTodo(todo.id)} 
+              className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
+    </ul>
+  </>
+
 }
 
 export default App
