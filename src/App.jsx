5fde3b14a6eae78251b54c697d19524cbb5efb57
Raydan Bala <raydanb@gmail.com>
2023-04-24 22:31:11 -0700

    New Component: NewTodoForm.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
 import { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm";
 
 function App() {
-  const [newItem, setNewItem] = useState("");
   const [todos, setTodos] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false}
-    ]})
-    setNewItem("");
-  }
 
   function toggleTodo(id, completed) {
     setTodos(currentTodos => {
@@ -34,18 +24,7 @@ function App() {
 
   return <>
 
-    <form onSubmit={handleSubmit} className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item:</label>
-        <input 
-        value={newItem}
-        onChange={e => {setNewItem(e.target.value)}}
-        type="text"
-        id="item"
-        />
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <NewTodoForm setTodos={setTodos} />
     <h1 className="header">Todo List</h1>
     <ul className="list">
       {todos.length === 0 && "No Todos"}
---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function NewTodoForm( {setTodos} ) {
+  const [newItem, setNewItem] = useState("");
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false}
+    ]})
+    setNewItem("");
+  }
+  
+  return <>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item:</label>
+        <input 
+        value={newItem}
+        onChange={e => {setNewItem(e.target.value)}}
+        type="text"
+        id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  </>
+}
+
+// export default NewTodoForm;
