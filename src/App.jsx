81c5db077219198c207fa265c461d97e90cba6c8
Raydan Bala <raydanb@gmail.com>
2023-05-10 23:24:45 -0700

    created IncompleteTodos component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 import { CompletedTodos } from "./CompletedTodos";
+import { IncompleteTodos } from "./IncompleteTodos";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -24,41 +25,30 @@ function App() {
     })
   }
 
-  const incompleteTodos = todos.filter(todo => !todo.completed);
-
   return <>
 
     <NewTodoForm setTodos={setTodos} />
 
-    <TodoList 
-      todos={todos}
-      deleteTodo={deleteTodo}
-      toggleTodo={toggleTodo}  
-    />
+    <div className="display-flex flex-row-reverse">
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo}
+      />
 
-    <CompletedTodos 
-      todos={todos}
-      deleteTodo={deleteTodo}
-      toggleTodo={toggleTodo} 
-    />
+      <CompletedTodos 
+        todos={todos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo} 
+      />
 
-    <h1 className="text-xl">Incomplete Todo's</h1>
-    <ol>
-      {incompleteTodos.map(todo => {
-        return (
-          <li key={todo.id}>
-          <label>
-            <input 
-              type="checkbox" 
-              checked={todo.completed} 
-              onChange={e => toggleTodo(todo.id)}/>
-            {todo.title}
-          </label>
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        )
-      })}
-    </ol>
+      <IncompleteTodos 
+        todos={todos}
+        deleteTodo={deleteTodo}
+        toggleTodo={toggleTodo} 
+      />
+
+    </div>
 
   </>
 
---
+++ src/IncompleteTodos.jsx
@@ -0,0 +1,24 @@
+export function IncompleteTodos({ todos, toggleTodo, deleteTodo }) {
+
+  const incompleteTodos = todos.filter(todo => !todo.completed);
+
+  return <>
+    <h1 className="text-xl">Incomplete Todo's</h1>
+    <ol>
+      {incompleteTodos.map(todo => {
+        return (
+          <li key={todo.id}>
+          <label>
+            <input 
+              type="checkbox" 
+              checked={todo.completed} 
+              onChange={e => toggleTodo(todo.id)}/>
+            {todo.title}
+          </label>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        )
+      })}
+    </ol>
+  </>
+}
